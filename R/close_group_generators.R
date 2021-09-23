493afb18b443b692043dd1290e4963b400a1b90f
Nick Koning <nickwkoning@gmail.com>
2021-09-23 11:29:13 +0200

    Vastly sped up group closing based on generators

--- R/close_group_generators.R
+++ R/close_group_generators.R
@@ -6,31 +6,17 @@
 #' @keywords combine generators
 #' @export
 #' @examples
-#' close_group_generators(construct_0_generators(8), 1)
+#' close_group_generators(construct_0_generators(8))
 #'
 
-close_group_generators = function(generators, identity_index = NULL) {
-
+close_group_generators = function(generators) {
   n = nrow(generators)
   p = log2(n)
 
-  # removes the identity element
-  if (!is.null(identity_index)) {
-    generators = generators[,-identity_index]
-  }
+  group = matrix(rep(1, n), ncol = 1)
 
-  group = matrix(nrow = n, ncol = n)
-  group[, 1] = 1
-  group[, 2:(p + 1)] = generators
-
-  counter = p + 2
-  for (i in 2:p) {
-    comb_mat = combn(p, i) + 1
-    for (ii in 1:ncol(comb_mat)) {
-      comb = comb_mat[, ii]
-      group[, counter] =  apply(group[,comb], 1, prod)
-      counter = counter + 1
-    }
+  for (i in 2:ncol(generators)) {
+    group = close_group_element(group, generators[, i])
   }
 
   return(group)
