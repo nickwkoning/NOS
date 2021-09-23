4e4a056438787b61276841cd30dd49cb7faeb4b1
Nick Koning <nickwkoning@gmail.com>
2021-09-23 17:52:00 +0200

    Can now obtain smaller oracle subgroup of desired order, if it exists

--- R/construct_oracle_subgroup.R
+++ R/construct_oracle_subgroup.R
@@ -1,6 +1,7 @@
 #' Constructs the largest possible oracle subgroup
 #'
-#' Constructs a maximum order oracle subgroup for n observations
+#' Constructs an oracle subgroup for n observations of desired order,
+#' given that it exists
 #' @param n number of observations that is a power of 2
 #' @return an oracle subgroup as an p x n matrix, with elements in columns
 #' @keywords construct oracle subgroup
@@ -8,12 +9,15 @@
 #' @examples
 #' construct_oracle_subgroup(4)
 
-construct_oracle_subgroup = function(n) {
-  if(any(is.null(construct_oracle_generators(n)))) {
+construct_oracle_subgroup = function(n, max_order = n) {
+  oracle_generators = construct_oracle_generators(n)
+  if(any(is.null(oracle_generators))) {
     return(NULL)
   }
 
-  group = close_group_generators(construct_oracle_generators(n))
+  p = floor(log2(max_order)) + 1
+  p = min(p, ncol(oracle_generators))
+  group = close_group_generators(oracle_generators[, 1:p, drop = F])
 
   return(group)
 }
