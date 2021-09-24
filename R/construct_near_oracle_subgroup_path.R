015bbd01974e1c9c7bd7d0e9fad9a04123c9b800
Nick Koning <nickwkoning@gmail.com>
2021-09-24 11:12:43 +0200

    constructs an entire path of subgroups now

---
+++ R/construct_near_oracle_subgroup_path.R
@@ -0,0 +1,47 @@
+#' Construct a path of near oracle subgroups
+#'
+#' @param n number of signs per sign-flip element
+#' @param num_candidates number of candidates generated with each group expansion
+#' @param max_order maximum rank (log2(order)) of the group
+#' @param leak_fun the function used to compare groups
+#' @param group # optional. A starting group. Default is just the identity group
+#' @return a near oracle group
+#' @keywords near oracle group
+#' @export
+#' @examples
+#' construct_near_oracle_subgroup_path(4, 100, 10, leak_max)
+#'
+
+
+
+construct_near_oracle_subgroup_path = function(n, num_candidates, max_rank, leak_fun) {
+  subgroup_list = list()
+
+  oracle_exhausted = FALSE
+  counter = 1
+  for (max_order in 2^(0:max_rank)) {
+    if (2^n < max_order) {
+      break
+    }
+
+    if (!oracle_exhausted) {
+      group = construct_oracle_subgroup(n, max_order)
+      if (ncol(group) < max_order) {
+        oracle_exhausted = TRUE
+      }
+    }
+
+    if (oracle_exhausted) {
+      group = subgroup_list[[counter - 1]]
+      subgroup_list[[counter]] = leak_minimizing_group_expansion_batched(group,
+                                                                         num_candidates,
+                                                                         leak_fun)
+    } else {
+      subgroup_list[[counter]] = group
+    }
+
+    counter = counter + 1
+
+  }
+  return(subgroup_list)
+}
